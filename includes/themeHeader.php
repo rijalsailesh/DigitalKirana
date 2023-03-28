4e26394995f009cde4f3a048e702c7c9c3d6471a
Sailesh Rijal <saileshrijal1@gmail.com>
2023-03-28 07:13:56 +0545

    header profile pic issue solved

--- includes/themeHeader.php
+++ includes/themeHeader.php
@@ -353,7 +353,7 @@ $tenant = getTenantById(getTenantId());
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $loggedInUser['Role'] . ": " ?><?= $loggedInUser['FirstName'] . " " . $loggedInUser['LastName'] ?></span>
-                                <img class="img-profile rounded-circle" src="/assets/theme/img/undraw_profile.svg">
+                                <img class="img-profile rounded-circle" src="/assets/imgs/logos/<?=$tenant['LogoUrl']==null?"default.png":$tenant['LogoUrl']?>">
                             </a>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
